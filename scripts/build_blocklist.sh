9edd837f1682843bdbeaad2309c617eb304a4df2
phynias <phynias@gmail.com>
2021-02-22 13:19:41 -0800

    stuff

---
+++ scripts/build_blocklist.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+curl -s https://www.iblocklist.com/lists.json | \
+    jq -r '.lists[] | .name as $n | select((.subscription == "false") and ($n | startswith("iana-") | not) and (["fornonlancomputers", "bogon", "The Onion Router"] | index($n) | not)) | .list' | \
+    awk 'length($0) > 2 { print "http://list.iblocklist.com/?fileformat=p2p&archiveformat=gz&list=" $0 }' | \
+    xargs wget -O - | \
+    gunzip | \
+    egrep -v '^#' | \
+    gzip > /zpool1/docker/static/files/blocklist.gz && \
+    chown nasuser.nasuser /zpool1/docker/static/files/blocklist.gz && \
+    cmod 664 /zpool1/docker/static/files/blocklist.gz
