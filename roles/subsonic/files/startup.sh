ee4002f402ed60d28945c2445c631a75705e0382
phynias <phynias@gmail.com>
2021-09-19 11:37:17 -0700

    added funkwhale, librephoto

--- roles/subsonic/files/startup.sh
+++ roles/subsonic/files/startup.sh
@@ -2,6 +2,7 @@
 HOME=/data
 HOST=0.0.0.0
 PORT=4040
+SSL_PORT=4141
 CONTEXT_PATH=/
 MAX_MEMORY=200
 MUSIC_FOLDER=/music
@@ -17,6 +18,7 @@ export LC_ALL=en_US.UTF-8
 /opt/subsonic/subsonic.sh --home=$HOME \
                   --host=$HOST \
                   --port=$PORT \
+	          --https-port=$SSL_PORT \
                   --max-memory=$MAX_MEMORY \
                   --default-music-folder=$MUSIC_FOLDER \
 		  --default-podcast-folder=$PODCAST_FOLDER \
