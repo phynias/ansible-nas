442c6635a9bc1e4ded1836bcdae16faef8d8fb6e
phynias <phynias@gmail.com>
2022-11-19 17:10:25 -0800

    added authelia. redid some network stuff (not done)

--- relaunch-vpn.sh
+++ relaunch-vpn.sh
@@ -4,10 +4,10 @@ cd /home/jackfrost/git/ansible-nas/
 if [ "$1" == "all" ]; then
   echo "Relaunching VPN container."
   docker pull misioslav/expressvpn:latest
-  ansible-playbook -i inventories/my-ansible-nas/inventory -D  -e docker_recreate=1 -t expressvpn  nas.yml
-  echo "expressvpn done..."
+  ansible-playbook --ask-vault-pass -i inventories/my-ansible-nas/inventory -D  -e docker_recreate=1 -t expressvpn  nas.yml
+  echo "Waiting for container to start."
+  sleep 10
 fi
 
-sleep 10
-echo "stuff"
-ansible-playbook -i inventories/my-ansible-nas/inventory -D  -e docker_recreate=1 -t traefik,sonarr,radarr,lidarr,bazarr,prowlarr,webhookd,papermerge,librephotos,transmission,sabnzbd,librenms,papermerge,webhookd nas.yml 
+
+ansible-playbook --ask-vault-pass -i inventories/my-ansible-nas/inventory -D  -e docker_recreate=1 -t traefik,sonarr,radarr,lidarr,bazarr,prowlarr,webhookd,papermerge,librephotos,transmission,sabnzbd,librenms,papermerge,webhookd nas.yml 
